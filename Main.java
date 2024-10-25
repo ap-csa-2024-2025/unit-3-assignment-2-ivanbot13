7fe4c6f70b34acec7deb0399cf93f5e01fe8d65a
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-10-25 11:17:18 -0700

    prob3

--- Main.java
+++ Main.java
@@ -4,6 +4,32 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    // prob1:
+    double prob1 = 12.345;
+    System.out.println("Please enter a double: ");
+    double duo = sc.nextDouble();
+    if (prob1 == duo)
+    {
+      System.out.println("YES");
+    }
+
+    //prob 2:
+    int prob2 = 48;
+    System.out.println("Please enter an integer: ");
+    int num = sc.nextInt();
+    if (prob2 == num)
+    {
+      System.out.println("YES");
+    }
+
+    // prob3:
+    System.out.println("Please enter 2 integers: ");
+    num = sc.nextInt();
+    int num2 = sc.nextInt();
+    if (num2 == (num * 2))
+    {
+      System.out.println("YES");
+    }
   }
 }
